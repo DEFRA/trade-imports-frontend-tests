629d7067faf63cf4eba122f3fd1cad70785dc901
Phil Morgan <phil@psmorgan.co.uk>
2025-06-26 09:36:08 +0100

    CDMS-713: Copy CDP's config file and packages
    We're still getting errors.
    
    Our repo differs slightly to the cdp-example-tests repo which CDP state does work with BrowserStack.
    This copies their browserstack config and packages to see if something we have is broken.

--- wdio.browserstack.conf.js
+++ wdio.browserstack.conf.js
@@ -2,20 +2,15 @@ import fs from 'node:fs'
 import { ProxyAgent, setGlobalDispatcher } from 'undici'
 import { bootstrap } from 'global-agent'
 
-/**
- * Enable webdriver.io to use the outbound proxy.
- * This is required for the test suite to be able to talk to BrowserStack.
- */
-if (process.env.CDP_HTTP_PROXY) {
-  const dispatcher = new ProxyAgent({
-    uri: 'http://localhost:3128'
-  })
-  setGlobalDispatcher(dispatcher)
-  bootstrap()
-  global.GLOBAL_AGENT.HTTP_PROXY = 'http://localhost:3128'
-}
+const debug = process.env.DEBUG
+const oneHour = 60 * 60 * 1000
 
-const oneMinute = 60 * 1000
+const dispatcher = new ProxyAgent({
+  uri: 'http://localhost:3128'
+})
+setGlobalDispatcher(dispatcher)
+bootstrap()
+global.GLOBAL_AGENT.HTTP_PROXY = 'http://localhost:3128'
 
 export const config = {
   //
@@ -23,31 +18,32 @@ export const config = {
   // Runner Configuration
   // ====================
   // WebdriverIO supports running e2e tests as well as unit and component tests.
-  runner: 'local',
-  //
+  runner: 'local', //
   // Set a base URL in order to shorten url command calls. If your `url` parameter starts
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
   baseUrl: `https://btms-portal-frontend.${process.env.ENVIRONMENT}.cdp-int.defra.cloud`,
 
-  // You will need to provide your own BrowserStack credentials.
-  // These should be added as secrets to the test suite.
   user: process.env.BROWSERSTACK_USERNAME,
   key: process.env.BROWSERSTACK_KEY,
 
   // Tests to run
-  specs: ['./test/specs/**/*.js'],
-  // Tests to exclude
+  specs: ['./test/specs/**/*.js'], // Tests to exclude
   exclude: [],
   maxInstances: 1,
+
+  // Presently calls outside of the cdp environment will be dropped.
+  // In chrome this can result in pages never finishing loading and the test step timing out.
+  // Going forward we will be making the outbound proxy available to test suite, but until that happens
+  // the work-around here is to add the external host names to the --host-resolver-rule below.
+  // This causes the calls to fail instantly rather than timeout.
 
   commonCapabilities: {
     'bstack:options': {
       buildName: `trade-imports-frontend-tests-${process.env.ENVIRONMENT}` // configure as required
     }
   },
-
   capabilities: [
     {
       browserName: 'Chrome', // Set as required
@@ -91,7 +87,7 @@ export const config = {
         testObservabilityOptions: {
           user: process.env.BROWSERSTACK_USER,
           key: process.env.BROWSERSTACK_KEY,
-          projectName: 'trade-imports-frontend-tests', // should match project in browserstack
+          projectName: 'trade-imports-frontend-tests',
           buildName: `trade-imports-frontend-tests-${process.env.ENVIRONMENT}`
         },
         acceptInsecureCerts: true,
@@ -105,16 +101,16 @@ export const config = {
     ]
   ],
 
-  execArgv: ['--loader', 'esm-module-alias/loader'],
+  execArgv: debug ? ['--inspect'] : [],
 
-  logLevel: 'info',
+  logLevel: debug ? 'debug' : 'info',
 
   // Number of failures before the test suite bails.
   bail: 0,
-  waitforTimeout: 20000,
-  waitforInterval: 200,
-  connectionRetryTimeout: 60000,
-  connectionRetryCount: 3,
+  waitforTimeout: 1000,
+  waitforInterval: 300,
+  connectionRetryTimeout: 12000,
+  connectionRetryCount: 1,
 
   framework: 'mocha',
 
@@ -141,18 +137,16 @@ export const config = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: 'bdd',
-    timeout: oneMinute
-  },
-
+    timeout: debug ? oneHour : 60000
+  }, //
+  // =====
   // Hooks
   afterTest: async function (
     test,
     context,
     { error, result, duration, passed, retries }
   ) {
-    if (error) {
-      await browser.takeScreenshot()
-    }
+    await browser.takeScreenshot()
   },
 
   onComplete: function (exitCode, config, capabilities, results) {
