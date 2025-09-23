0a53bafdd687870a97cfd71a0a390ad2c25f5483
Mohammed Shahin Uddin <mo@thetestroom.com>
2025-09-23 13:56:05 +0100

    CDMS-1055: Test for Reporting page (#33)

--- test/page-objects/reporting.page.js
+++ test/page-objects/reporting.page.js
@@ -4,6 +4,54 @@ class ReportingPage extends Page {
   open() {
     return super.open('/reporting')
   }
+
+  get todayLink() {
+    return $('.btms-reporting-filters a')
+  }
+
+  get filterResultText() {
+    return $('.govuk-grid-column-three-quarters .govuk-heading-s')
+  }
+
+  get matches() {
+    return $('section[aria-labelledby="matches"]')
+  }
+
+  get releases() {
+    return $('section[aria-labelledby="releases"]')
+  }
+
+  get clearanceRequests() {
+    return $('section[aria-labelledby="clearanceRequests"]')
+  }
+
+  get notifications() {
+    return $('section[aria-labelledby="notifications"]')
+  }
+
+  async todayFilter() {
+    return await this.clickLink(this.todayLink)
+  }
+
+  async filterResult() {
+    return await this.getTextFrom(this.filterResultText)
+  }
+
+  async matchesSectionIsVisible() {
+    return await this.elementIsDisplayed(this.matches)
+  }
+
+  async releasesSectionIsVisible() {
+    return await this.elementIsDisplayed(this.matches)
+  }
+
+  async clearanceRequestSectionIsVisible() {
+    return await this.elementIsDisplayed(this.matches)
+  }
+
+  async notificationSectionIsVisible() {
+    return await this.elementIsDisplayed(this.matches)
+  }
 }
 
 export default new ReportingPage()
