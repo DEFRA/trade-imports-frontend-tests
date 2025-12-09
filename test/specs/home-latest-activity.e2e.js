7ea05b477cdb21ea81a0cb2010747c09cf675c13
Mohammed Shahin Uddin <mo@thetestroom.com>
2025-12-09 14:41:53 +0000

    CDMS-626: Combined Latest Activity tests with home page tests

--- test/specs/home-latest-activity.e2e.js
+++ test/specs/home-latest-activity.e2e.js
@@ -1,11 +1,25 @@
 import { expect } from '@wdio/globals'
 import HomePage from '../page-objects/home.page.js'
+import LatestActivityPage from '../page-objects/latest-activity.page.js'
 import LatestActivityPage from '../page-objects/latest-activity.page'
 import SearchPage from 'page-objects/search.page.js'
 
-describe('Latest Activity Page', () => {
-  it('should display BTMS, CDS, and IPAFFS headers, all message rows, and valid date fields', async () => {
+describe('Home and Latest Activity Page', () => {
+  it('Should be on the "Home" page', async () => {
     await HomePage.open()
+    await expect(browser).toHaveTitle(
+      'Border Trade Matching Service - Border Trade Matching Service'
+    )
+  })
+
+  it('Should be able to see list of registered users', async () => {
+    await HomePage.login()
+    await HomePage.gatewayLogin()
+    await expect(browser).toHaveTitle('DEFRA ID Login | cdp-defra-id-stub')
+  })
+
+  it('should display BTMS, CDS, and IPAFFS headers, all message rows, and valid date fields', async () => {
+    await HomePage.loginRegisteredUser()
 
     if (!(await SearchPage.sessionActive())) {
       await HomePage.login()
