f0400bc97493fb7b6191aacdf853b248cc099570
Mohammed Shahin Uddin <mo@thetestroom.com>
2026-02-17 11:42:39 +0000

    CDMS-1313: Updating tests GMR when customs or transits are null (#82)

--- test/specs/gmr-page-checks.e2e.js
+++ test/specs/gmr-page-checks.e2e.js
@@ -3,6 +3,7 @@ import { sendCdsMessageFromFile } from '../utils/soapMessageHandler.js'
 import { sendGmrMessageFromFile } from '../utils/gmrMessageHandler.js'
 
 import HomePage from 'page-objects/home.page'
+import GmrSearchResultsPage from '../page-objects/gmr-search-results.page.js'
 import SearchResultsPage from '../page-objects/searchResultsPage.js'
 import SearchPage from 'page-objects/search.page.js'
 
@@ -10,8 +11,23 @@ describe('Search Results Page for GMR Page and GMR Links', () => {
   before(async () => {
     await sendCdsMessageFromFile('../data/gmr/clearance-gmr.xml')
     await sendCdsMessageFromFile('../data/gmr/clearance-gmr-1.xml')
+    await sendCdsMessageFromFile('../data/gmr/1-clearance-gmr-customs.xml')
+    await sendCdsMessageFromFile('../data/gmr/2-clearance-gmr-transit.xml')
+    await sendCdsMessageFromFile('../data/gmr/3-clearance-gmr-customs-null.xml')
+    await sendCdsMessageFromFile('../data/gmr/4-clearance-gmr-transit-null.xml')
+    await sendCdsMessageFromFile(
+      '../data/gmr/4-clearance-gmr-transit-null-2.xml'
+    )
     await sendIpaffMessageFromFile('../data/gmr/ipaff-gmr.json')
+    await sendIpaffMessageFromFile('../data/gmr/1-ipaff-gmr-customs.json')
+    await sendIpaffMessageFromFile('../data/gmr/2-ipaff-gmr-transit.json')
+    await sendIpaffMessageFromFile('../data/gmr/3-ipaff-gmr-customs-null.json')
+    await sendIpaffMessageFromFile('../data/gmr/4-ipaff-gmr-transit-null.json')
     await sendGmrMessageFromFile('../data/gmr/gmr.json')
+    await sendGmrMessageFromFile('../data/gmr/1-gmr-empty-customs.json')
+    await sendGmrMessageFromFile('../data/gmr/2-gmr-empty-transit.json')
+    await sendGmrMessageFromFile('../data/gmr/3-gmr-null-customs.json')
+    await sendGmrMessageFromFile('../data/gmr/4-gmr-null-transit.json')
     await HomePage.open()
 
     if (!(await SearchPage.sessionActive())) {
@@ -37,5 +53,53 @@ describe('Search Results Page for GMR Page and GMR Links', () => {
     expect(
       await SearchResultsPage.isGmrLinkDisplayedForMrn('24GBBGBKCDMS135014')
     ).toBe(false)
+  })
+
+  it('Should see GMR and MRN when customs is empty', async () => {
+    const gmrId = 'GMRA11350002'
+    const mrnId = '24GBBGBKCDMS135015'
+    await SearchPage.open()
+    await SearchPage.search(gmrId)
+    expect(await GmrSearchResultsPage.getDisplayedGmr()).toBe(
+      `Showing result for\n${gmrId}`
+    )
+    await GmrSearchResultsPage.clickLinkedMrn(mrnId)
+    expect(await SearchResultsPage.getResultText()).toContain(mrnId)
+  })
+
+  it('Should see GMR and MRN when transit is empty', async () => {
+    const gmrId = 'GMRA11350004'
+    const mrnId = '24GBBGBKCDMS135016'
+    await SearchPage.open()
+    await SearchPage.search(gmrId)
+    expect(await GmrSearchResultsPage.getDisplayedGmr()).toBe(
+      `Showing result for\n${gmrId}`
+    )
+    await GmrSearchResultsPage.clickLinkedMrn(mrnId)
+    expect(await SearchResultsPage.getResultText()).toContain(mrnId)
+  })
+
+  it('Should see GMR and MRN when customs is null', async () => {
+    const gmrId = 'GMRA11350005'
+    const mrnId = '24GBBGBKCDMS135017'
+    await SearchPage.open()
+    await SearchPage.search(gmrId)
+    expect(await GmrSearchResultsPage.getDisplayedGmr()).toBe(
+      `Showing result for\n${gmrId}`
+    )
+    await GmrSearchResultsPage.clickLinkedMrn(mrnId)
+    expect(await SearchResultsPage.getResultText()).toContain(mrnId)
+  })
+
+  it('Should see GMR and MRN when transit is null', async () => {
+    const gmrId = 'GMRA11350006'
+    const mrnId = '24GBBGBKCDMS135018'
+    await SearchPage.open()
+    await SearchPage.search(gmrId)
+    expect(await GmrSearchResultsPage.getDisplayedGmr()).toBe(
+      `Showing result for\n${gmrId}`
+    )
+    await GmrSearchResultsPage.clickLinkedMrn(mrnId)
+    expect(await SearchResultsPage.getResultText()).toContain(mrnId)
   })
 })
