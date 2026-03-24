9424dec16e604ec314ab401b144ce1ccfcd423eb
Mohammed Shahin Uddin <mo@thetestroom.com>
2026-03-24 16:26:51 +0000

    CDMS-1345: Test to capture GMR and CSV download redirection

--- test/specs/page-checks.e2e.js
+++ test/specs/page-checks.e2e.js
@@ -4,11 +4,14 @@ import HomePage from '../page-objects/home.page.js'
 import SearchPage from '../page-objects/search.page.js'
 import SearchResultsPage from '../page-objects/searchResultsPage.js'
 import LatestActivityPage from '../page-objects/latest-activity.page.js'
+import GmrSearchResultsPage from '../page-objects/gmr-search-results.page.js'
 import { sendCdsMessageFromFile } from '../utils/soapMessageHandler.js'
+import { sendGmrMessageFromFile } from '../utils/gmrMessageHandler.js'
 
 describe('Page Redirection', () => {
   before(async () => {
     await sendCdsMessageFromFile('../data/search/cds.xml')
+    await sendGmrMessageFromFile('../data/gmr/gmr.json')
 
     await HomePage.open()
     if (await SearchPage.sessionActive()) {
@@ -33,7 +36,7 @@ describe('Page Redirection', () => {
     expect(await SearchPage.isSearchButtonVisible()).toBe(true)
   })
 
-  it('Should get redirected when going to the Search Results Page without logging in', async () => {
+  it('Should get redirected when going to the Search Results Page for an MRN without logging in', async () => {
     await HomePage.openPage('/search-result?searchTerm=24GBBGBKCDMS704709')
     expect(await HomePage.isGatewayRadioButtonVisible()).toBe(true)
 
@@ -42,6 +45,18 @@ describe('Page Redirection', () => {
     await HomePage.openPage('/search-result?searchTerm=24GBBGBKCDMS704709')
     expect(await SearchResultsPage.getResultText()).toContain(
       '24GBBGBKCDMS704709'
+    )
+  })
+
+  it('Should get redirected when going to the Search Results Page for an GMR without logging in', async () => {
+    await HomePage.openPage('/gmr-search-result?searchTerm=GMRA11350001')
+    expect(await HomePage.isGatewayRadioButtonVisible()).toBe(true)
+
+    await HomePage.gatewayLogin()
+    await HomePage.loginRegisteredUser()
+    await HomePage.openPage('/gmr-search-result?searchTerm=GMRA11350001')
+    expect(await GmrSearchResultsPage.getDisplayedGmr()).toBe(
+      `Showing result for\nGMRA11350001`
     )
   })
 
@@ -77,6 +92,20 @@ describe('Page Redirection', () => {
     await expect(browser).toHaveTitle(
       'You do not have the correct permissions to access this service - Border Trade Matching Service'
     )
+  })
+
+  it('Should get redirected when trying to download a manual release report', async () => {
+    await HomePage.openPage(
+      '/reporting/manual-releases.csv?startDate=24%2F3%2F2026&endDate=24%2F3%2F2026'
+    )
+    expect(await HomePage.isGatewayRadioButtonVisible()).toBe(true)
+
+    await HomePage.gatewayLogin()
+    await HomePage.loginRegisteredUser()
+    await HomePage.openPage(
+      '/reporting/manual-releases.csv?startDate=24%2F3%2F2026&endDate=24%2F3%2F2026'
+    )
+    expect(await HomePage.isGatewayRadioButtonVisibleWithoutCheck()).toBe(false)
   })
 
   it('Should get redirected when trying to download a no match report', async () => {
