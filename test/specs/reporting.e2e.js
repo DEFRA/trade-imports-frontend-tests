8533a1a6405cf5de5381477e382d7ab89c1c5f7f
Chris Johnson <chrisjohnson@equalexperts.com>
2026-05-11 09:29:22 +0100

    Add lastMonthFilter() so that tests work with data created today (for clean local envs) (#94)
    Added last month filter before opening the summary tab in two test cases to ensure accurate data retrieval.

--- test/specs/reporting.e2e.js
+++ test/specs/reporting.e2e.js
@@ -89,6 +89,7 @@ describe('Reporting page', () => {
   })
 
   it('Should show CSV download links for No matches and Manual releases and they are clickable', async () => {
+    await ReportingPage.lastMonthFilter()
     await ReportingPage.openSummaryTab()
 
     const noMatchesVisible =
@@ -323,6 +324,7 @@ describe('Reporting page', () => {
   })
 
   it('Should validate percentage calculations across Summary tiles', async () => {
+    await ReportingPage.lastMonthFilter()
     await ReportingPage.openSummaryTab()
 
     const matchesPct = await ReportingPage.getSummaryMatchesPercentage()
